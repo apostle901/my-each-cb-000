bf80001867736adc5b3e389dd6f4c8a9b6fe67d9
Jawann <jawann@gmail.com>
2017-11-27 03:41:06 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each (array)# put argument(s) here
   # code here
+  if block_given?
+    idx = 0
+    while idx < array.length - 1 do
+      yield (array[idx])
+      idx += 1
+    end
+  end
+  array
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
